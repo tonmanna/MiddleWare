b69343b177ccc68c9b8ea361f84ee65e6ebfb208
Worawut <tonmanna@gmail.com>
2013-08-25 22:16:50 +0700

    Connect Example

---
+++ connect.js
@@ -0,0 +1,11 @@
+﻿var http = require('http');
+var connect = require('connect');
+var Q = require('Q');
+
+var app = connect()
+  .use(connect.logger('dev'))
+  .use(connect.static('public'))
+  .use(function (req, res) {
+      res.end('hello world\n');
+  })
+ .listen(3000);
