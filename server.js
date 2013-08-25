627107218cbb89bf3678fa1e70f57c7f5bff6d21
Worawut <tonmanna@gmail.com>
2013-08-25 07:22:57 +0700

    Vow

--- server.js
+++ server.js
@@ -1,36 +1,4 @@
 ﻿var http = require('http');
 var Q = require('Q');
+var Vow = require('vow');
 
-function my(i)
-{
-    return "MyLoog" + i;
-}
-
-
-
-    /*
-    Q.fcall(function () {
-        for (var i = 0; i < 10; i++) {
-            setTimeout(console.log("MyLoog" + i), 1000);
-        }
-        console.log("End Loop");
-        setTimeout(console.log("1"), 1000);
-    })
-    .then(function () { setTimeout(console.log("2"), 500); })
-    .then(function () { setTimeout(console.log("3"), 250); })
-    .then(function () { setTimeout(console.log("4"), 125); })
-    .then(function (value4) {
-        // Do something with value4
-        setTimeout(console.log(value4), 100);
-    }, function (error) {
-        // Handle any error from step1 through step4
-        setTimeout(console.log("ERROR"), 100);
-    })
-    .done(function () { setTimeout(console.log("Done"), 1000); });
-    */
-
-    http.createServer(function (req, res) {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('Hello World\n');
-    }).listen(3000, '127.0.0.1');
-    console.log('Server running at http://127.0.0.1:1337/');
