c22e4664c4d4a63b8a60be52f5d1c9f6c226862d
Worawut <tonmanna@gmail.com>
2013-08-25 08:04:23 +0700

    First Q

--- server.js
+++ server.js
@@ -1,4 +1,41 @@
 ﻿var http = require('http');
 var Q = require('Q');
-var Vow = require('vow');
 
+function getconnection(conectionstring) {
+    return Q.fcall(function () {
+        return "Connection";
+    });
+}
+function getCon(model) {
+    return Q.fcall(function () {
+        return "Table";
+    });
+}
+
+function getData(Table) {
+    return Q.fcall(function () {
+        return "Data";
+    });
+}
+
+
+getconnection("Connectionstring")
+.then(getCon)
+.then(getData)
+.then(function(value){
+    console.log(value);
+})
+.catch(function (error) {
+    // Handle any error from all above steps
+    console.log("ERROR");
+})
+.done();
+
+/*
+return Q.fcall(function () {
+    return 10;
+});
+return Q.fcall(function () {
+    throw new Error("Can't do it");
+});
+*/
